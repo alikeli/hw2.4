617b6b92bf00dc0c5eb98d3f4212b1be5f2cd732
alikeli <eli-com2@yandex.ru>
2022-09-20 11:59:10 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course/course2lesson4/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course.course2lesson4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
